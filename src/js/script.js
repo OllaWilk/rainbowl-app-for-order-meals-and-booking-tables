1aab3c334c4e21334f4795477f75483fbd29cefa
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2022-12-22 23:11:33 +0100

    init test product

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-console.log('script');
+{
+  ('use strict');
+
+  class Product {
+    constructor() {
+      console.log('new PRODUCT', this);
+    }
+  }
+
+  const app = {
+    initMenu: function () {
+      const testProduct = new Product();
+      console.log('testProduct', testProduct);
+    },
+    init: function () {
+      console.log('***START APP***');
+      this.initMenu();
+    },
+  };
+
+  app.init();
+}
